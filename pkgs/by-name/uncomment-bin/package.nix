50b87be9e2f57a4d9434c04d8aa7ca1471f12013
Cameron Smith <cameron.ray.smith@gmail.com>
2026-09-03 20:33:52 -0400

    uncomment-bin: 3.0.3 -> 3.6.0
    Changelog: https://github.com/Goldziher/uncomment/releases/tag/v3.6.0
    Change-Id: I029dfa6171e3ccbf5d9c74f13851283000434676

--- pkgs/by-name/uncomment-bin/package.nix
+++ pkgs/by-name/uncomment-bin/package.nix
@@ -15,26 +15,26 @@ let
   systemToPlatform = {
     "x86_64-linux" = {
       name = "x86_64-unknown-linux-gnu";
-      hash = "sha256-eoMb0G0wCLOjIQczGGzUjpAtwfuv7DLMdS+4iZhoAlc=";
+      hash = "sha256-/zo/r7sBHRs14AEd6gbpiDwkgGiLatuCZ6GhE54V0oE=";
     };
     "aarch64-linux" = {
       name = "aarch64-unknown-linux-gnu";
-      hash = "sha256-QI+2+iSQVHO9PihNACxX/JjqZJp7bOYtgrKzqS1ISoE=";
+      hash = "sha256-j1JAdbCjH7+Ml62S3lzc9ozTcGJ+74Gzer72X/zUffs=";
     };
     "x86_64-darwin" = {
       name = "x86_64-apple-darwin";
-      hash = "sha256-iJGed8UCX/fptnlfvnu/S86e2g/41HVKHKW1uwDej+I=";
+      hash = "sha256-WevGkp3cvKeT+XjzvyeewFxFZUZA60eAyUQBRGHFW8k=";
     };
     "aarch64-darwin" = {
       name = "aarch64-apple-darwin";
-      hash = "sha256-A6AKGfwKlL1PbXUMsZWmp3IBc4+bfaCbZSL1rkGTjkM=";
+      hash = "sha256-gyTLdYKpzO06R3B1fzBzpKEo4+DpT0UwY+y0l54fyMU=";
     };
   };
   platform = systemToPlatform.${system} or (throw "uncomment-bin: unsupported platform ${system}");
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "uncomment-bin";
-  version = "3.0.3";
+  version = "3.6.0";
 
   src = fetchurl {
     url = "https://github.com/Goldziher/uncomment/releases/download/v${finalAttrs.version}/uncomment-${platform.name}.tar.gz";
@@ -48,11 +48,14 @@ stdenv.mkDerivation (finalAttrs: {
   dontConfigure = true;
   dontBuild = true;
 
-  # Linux artifacts are dynamically-linked glibc PIEs (NEEDED: libc, libpthread,
-  # libdl only -- no libgcc_s/libstdc++/openssl); autoPatchelfHook rewrites the
-  # interpreter with no extra buildInputs. Darwin artifacts are ad-hoc signed
-  # Mach-O and need no patching.
+  # Required, not defensive: the Linux artifacts are glibc-dynamic PIEs
+  # (NEEDED: libc.so.6, libdl.so.2, libgcc_s.so.1, libm.so.6, libpthread.so.0),
+  # so both the interpreter and the libgcc_s lookup need rewriting. Darwin
+  # artifacts are ad-hoc signed Mach-O and need no patching.
   nativeBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [ autoPatchelfHook ];
+  buildInputs = lib.optionals stdenv.hostPlatform.isLinux [
+    stdenv.cc.cc.lib # libgcc_s.so.1
+  ];
 
   installPhase = ''
     runHook preInstall
